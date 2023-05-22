466c58befceda4c2bff3475d48e5a70d0104e06b
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-21 23:56:35 -0500

    Initial commit

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
+import { CrearProductoComponent } from './pagina/crear-producto/crear-producto.component';
+const routes: Routes = [
+{ path: "", component: InicioComponent },
+{ path: "login", component: LoginComponent },
+{ path: "registro", component: RegistroComponent },
+{ path: "crear", component: CrearProductoComponent},
+{ path: "**", pathMatch: "full", redirectTo: "" }
+];
+
+@NgModule({
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
+import { CrearProductoComponent } from './pagina/crear-producto/crear-producto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegistroComponent,
+    CrearProductoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
