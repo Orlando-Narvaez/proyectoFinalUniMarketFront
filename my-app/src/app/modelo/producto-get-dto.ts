d87e0dfb8fb4e418afa525eb3521c1e0fb784f2b
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-05-23 08:13:00 -0500

    Create producto-get-dto.ts
    se crean las variables en producto get dto

---
+++ my-app/src/app/modelo/producto-get-dto.ts
@@ -0,0 +1,10 @@
+export class ProductoGetDto 
+{
+    codigo: string= "";
+    nombre: string = "";
+    descripcion: string = "";
+    precio: number = 0;
+    unidades: number = 0;
+    imagenes: string[] = [];
+    categoria: string = "";
+}
