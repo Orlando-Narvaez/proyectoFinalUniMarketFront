727b309f938af215b105ec87bf8fe463280edb7a
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-05-23 08:12:43 -0500

    Create producto-get-dto.spec.ts
    Se crea la clase producto get dto

---
+++ my-app/src/app/modelo/producto-get-dto.spec.ts
@@ -0,0 +1,7 @@
+import { ProductoGetDto } from './producto-get-dto';
+
+describe('ProductoGetDto', () => {
+  it('should create an instance', () => {
+    expect(new ProductoGetDto()).toBeTruthy();
+  });
+});
