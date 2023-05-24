7bdc45437d81beeda00cacbef6c6130d09b66e3f
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-24 18:08:46 -0500

    Update app-routing.module.ts

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
 import { RegistroComponent } from './pagina/registro/registro.component';
 import { CrearProductoComponent } from './pagina/crear-producto/crear-producto.component';
+import { BusquedaComponent } from './pagina/busqueda/busqueda.component';
 const routes: Routes = [
 { path: "", component: InicioComponent },
 { path: "login", component: LoginComponent },
 { path: "registro", component: RegistroComponent },
 { path: "crear", component: CrearProductoComponent},
-{ path: "**", pathMatch: "full", redirectTo: "" }
+{ path: "**", pathMatch: "full", redirectTo: "" },
 { path: "busqueda/:texto", component: BusquedaComponent }
 ];
 
