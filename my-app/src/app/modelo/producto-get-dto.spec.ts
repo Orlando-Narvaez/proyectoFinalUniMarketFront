246433f150fddf4b7612feef3860a51860992007
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-24 18:08:55 -0500

    Delete producto-get-dto.spec.ts

--- my-app/src/app/modelo/producto-get-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProductoGetDto } from './producto-get-dto';
-
-describe('ProductoGetDto', () => {
-  it('should create an instance', () => {
-    expect(new ProductoGetDto()).toBeTruthy();
-  });
-});
