aa44d9d251c73848efbc0647b9be2ec47a6fbf79
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-24 18:08:58 -0500

    Update producto-get-dto.ts

--- my-app/src/app/modelo/producto-get-dto.ts
+++ my-app/src/app/modelo/producto-get-dto.ts
@@ -1,10 +1,21 @@
-export class ProductoGetDto 
+export class ProductoGetDTO 
 {
-    codigo: string= "";
+    codigo: number= 0;
     nombre: string = "";
     descripcion: string = "";
     precio: number = 0;
     unidades: number = 0;
     imagenes: string[] = [];
-    categoria: string = "";
+    categoria: string [] = [];
+
+    constructor(codigo: number,nombre: string, descripcion: string, precio: number, unidades: number, imagenes: string[], categoria: string [])
+    {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.unidades = unidades;
+        this.imagenes = imagenes;
+        this.categoria = categoria;
+    }
 }
