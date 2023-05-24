cc8c816547a22b708da6aef614e2d8ca293b220f
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-24 18:08:51 -0500

    Update app.component.ts

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent 
+{
   title = 'my-app';
+
+  constructor(private router:Router)
+  {
+    this.router = router
+  }
+
+  public iraBusqueda(valor:string){
+    if(valor)
+    {
+      this.router.navigate(["/busqueda", valor]);
+    }
+  }
 }
