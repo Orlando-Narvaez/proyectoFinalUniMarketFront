1f15da36b66d48f7b3e5c503f014eaed785d4ff1
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-05-24 23:26:51 -0500

    Update producto.service.ts
    se crea ts

--- my-app/src/app/servicios/producto.service.ts
+++ my-app/src/app/servicios/producto.service.ts
@@ -1,5 +1,10 @@
 import { ProductoGetDTO } from "../modelo/producto-get-dto";
 
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class ProductoService 
 {
   productos:ProductoGetDTO[];
